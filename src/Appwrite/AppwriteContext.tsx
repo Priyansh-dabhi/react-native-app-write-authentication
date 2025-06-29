81880ac8d805fc1956ac04c1a33a367cf1d2864b
Priyansh-dabhi <priyanshdabhi7@gmail.com>
2025-06-29 22:51:18 +0530

    Add AppwriteContext to provide glotble Appwrite service

---
+++ src/Appwrite/AppwriteContext.tsx
@@ -0,0 +1,35 @@
+import { View, Text } from 'react-native'
+import React, { createContext, FC } from 'react'
+import { PropsWithChildren , useState} from 'react'
+import Appwrite from './service'
+
+
+type AppContextType = {
+    appwrite:Appwrite
+    isLoggedIn : boolean
+    setIsLoggedIn:(isLoggedIn:boolean)=>void
+}
+
+export const AppwriteContext = createContext<AppContextType>({
+    // passing values
+    appwrite: new Appwrite(),
+    isLoggedIn : false,
+    setIsLoggedIn:()=>{}
+})
+
+export const AppwriteProvider: FC<PropsWithChildren> = ({children}) => {
+    
+        const [isLoggedIn,setIsLoggedIn] = useState(false);
+        const defauldtValue = {
+            appwrite: new Appwrite(),
+            isLoggedIn,
+            setIsLoggedIn
+        }
+    return (
+    <AppwriteContext.Provider value={defauldtValue}>
+      {children}
+    </AppwriteContext.Provider>
+  )
+}
+
+export default AppwriteContext
